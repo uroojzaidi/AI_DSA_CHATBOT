3605309c9779ab3ed8f8421a34930deebe43c2fb
uroojzaidi <urooj.zaidi777@gmail.com>
2025-11-14 16:10:26 +0500

    Add main.cpp with welcome message and API key

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "config.h"
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to AI_DSA_CHATBOT!" << endl;
+    cout << "API Key loaded: " << API_KEY << endl;
+    return 0;
+}
